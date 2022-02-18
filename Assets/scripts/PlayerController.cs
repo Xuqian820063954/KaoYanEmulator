f09f0ea4b08da1fc101fab59eab562d4a1fe920c
徐乾 <820063954@qq.com>
2022-02-18 22:44:56 +0800

    Initial commit

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D myRigidBody;
+    private Animator myAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+    void Run()
+    {
+        myRigidBody.velocity = new Vector2(0, 0);
+        myAnim.SetBool("Up", false);
+        myAnim.SetBool("Down", false);
+        myAnim.SetBool("Left", false);
+        myAnim.SetBool("Right", false);
+        float moveDirY = Input.GetAxis("Vertical");
+        Debug.Log(moveDirY);
+        if (moveDirY != 0)
+        {
+            Vector2 playerVel = new Vector2(0.0f, moveDirY * speed);
+            myRigidBody.velocity = playerVel;
+            if (moveDirY > Mathf.Epsilon)
+                myAnim.SetBool("Up", true);
+            else if (moveDirY < -Mathf.Epsilon)
+                myAnim.SetBool("Down", true);
+            return;
+        }
+        float moveDirX = Input.GetAxis("Horizontal");
+        if (moveDirX != 0)
+        {
+            Vector2 playerVel = new Vector2(moveDirX * speed, 0.0f);
+            myRigidBody.velocity = playerVel;
+            if (moveDirX > Mathf.Epsilon)
+                myAnim.SetBool("Right", true);
+            else if (moveDirX < -Mathf.Epsilon)
+                myAnim.SetBool("Left", true);
+        }
+    }
+}
---
+++ Assets/scripts/init.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class init : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnClick()
+    {
+        SceneManager.LoadScene("Init");
+    }
+}
